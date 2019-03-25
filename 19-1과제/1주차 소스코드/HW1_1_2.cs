e939d237d4e0f521877e497d1812577307b77787
seongjinbak <seongjinbak.sjb@gmail.com>
2019-03-26 01:45:27 +0900

    1주차 과제 1~2 소스코드
    1-1, 1-2, 2-1 소스코드

---
+++ 19-1과제/1주차 소스코드/HW1_1_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace CsharpPractice
+{
+    class HW1_1_2
+    {/*
+        static void Main(string[] args)
+        {
+            // 원의 반지름을 입력받음
+            double radius = double.Parse(Console.ReadLine());
+
+            // 실수 자료형(double)의 변수 PI 선언 및 할당
+            double PI = 3.14;
+
+            // 각 둘레와 넓이를 int 형으로 변환 후 출력.
+            Console.WriteLine("둘레 : " + (int)( 2 * PI * radius) + " 넓이 " + (int)(PI * radius * radius));
+        }
+        */
+    }
+}
---
+++ 19-1과제/1주차 소스코드/HW1_2_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace CsharpPractice
+{
+    class HW1_2_1
+    {/*
+        static void Main(string[] args)
+        {
+            // float 형 변수 a 와 b를 입력받음
+            float a = float.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+
+            // 각각 출력
+            Console.WriteLine("+ 한 결과 : " + (a + b));
+            Console.WriteLine("\n- 한 결과 : " + (a - b));
+            Console.WriteLine("\n* 한 결과 : " + (a * b));
+            Console.WriteLine("\n/ 한 결과 : " + (a / b));
+            Console.WriteLine("\n% 한 결과 : " + (a % b));
+            
+        }
+        */
+    }
+}
